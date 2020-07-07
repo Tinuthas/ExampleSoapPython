3b72755b6b272481f66aa4426ca5c535b0a9d866
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-07-07 13:06:05 -0300

    url e nfe

--- nfeExample.py
+++ nfeExample.py
@@ -5,8 +5,11 @@ from requests import Session
 
 #wsdl = 'http://www.soapclient.com/xml/soapresponder.wsdl'
 #wsdl = 'https://homologacao.nfe.fazenda.sp.gov.br/ws/nfeautorizacao4.asmx?wsdl'
-wsdl = 'https://homologacao.nfe.fazenda.sp.gov.br/ws/nfeautorizacao4.asmx'
+#wsdl = 'https://homologacao.nfe.fazenda.sp.gov.br/ws/nfeautorizacao4.asmx'
+wsdl = 'https://nfe.fazenda.sp.gov.br/ws/nfeautorizacao4.asmx​'
 
+#nota fiscal chave
+nfe = '35200643708379010830550050002904511102904515'
 
 session = Session()
 session.verify = 'path/to/my/certificate.pem'
