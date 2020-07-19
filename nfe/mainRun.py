17424790f74564ec3ba70ce8c0af82eda057b72c
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-07-19 11:41:57 -0300

    construction class to run NFE

---
+++ nfe/mainRun.py
@@ -0,0 +1,6 @@
+from cUF import UF
+from mod import MOD
+from tpEmis import TPEmis
+from runNF import getCertificate
+
+nfe = getCertificate(str(UF.SP.value), '20', '06', '43.708.379/0108-30', '55')
--- runNF.py
+++ runNF.py
@@ -1,3 +1,18 @@
+class Nfe:
+    numberSerie = ''
+    numberNF = ''
+    numberCH = ''
+    digitCH = ''
+
+    def __init__(self, uf: str, year: str, month: str, cnpj: str, mod: str):
+        self.uf = uf
+        self.year = year
+        self.month = month
+        self.cnpj = cnpj
+        self.mod = mod
+
+    def getCompleteNfe(self):
+        return self.uf + self.year + self.month + self.cnpj + self.mod + self.numberSerie + self.numberNF + self.numberCH + self.digitCH
 
 
 def getKey(uf: str, year: str, month: str, cnpj: str, mod: str):
@@ -15,7 +30,7 @@ def getKey(uf: str, year: str, month: str, cnpj: str, mod: str):
 
     print('\n Run NF')
 
-    #numberSerie
+    # numberSerie
     for i in range(0, 999):
         if '005' == '%003d' % i:
             numberSerie = '%003d' % i
@@ -46,3 +61,47 @@ def getKey(uf: str, year: str, month: str, cnpj: str, mod: str):
 
     return uf + year + month + cnpjFormat + mod + numberSerie + numberNF + numberCH + digitCH
 
+
+def getCertificate(uf: str, year: str, month: str, cnpj: str, mod: str):
+    cnpjFormat = str(int(cnpj.replace('.', '').replace('/', '').replace('-', '')))
+
+    numberNfe = Nfe(uf, year, month, cnpjFormat, mod)
+
+    getNumberSerie(numberNfe)
+
+    print('end')
+
+    return numberNfe
+
+
+def getNumberSerie(numberNfe: Nfe):
+    for i in range(0, 999):
+        numberNfe.numberSerie = '%003d' % i
+        print('%003d' % i)
+        getNumberNF(numberNfe)
+
+
+def getNumberNF(numberNfe: Nfe):
+    for i in range(0, 999999999):
+        numberNfe.numberNF = '%000000009d' % i
+        print('%000000009d' % i)
+        getNumberCH(numberNfe)
+
+
+def getNumberCH(numberNfe: Nfe):
+    for i in range(0, 999999999):
+        numberNfe.numberCH = '%000000009d' % i
+        print('%000000009d' % i)
+        getDigitCH(numberNfe)
+
+
+def getDigitCH(numberNfe: Nfe):
+    for i in range(0, 9):
+        numberNfe.digitCH = str(i)
+        print(i)
+        getClientSoap(numberNfe)
+
+
+def getClientSoap(numberNfe: Nfe):
+    # uf + year + month + cnpjFormat + mod + numberSerie + numberNF + numberCH + digitCH
+    print(numberNfe.getCompleteNfe())
